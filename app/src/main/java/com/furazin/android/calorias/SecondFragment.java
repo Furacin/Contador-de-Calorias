550a2aa7c4f57377604e9b373276c3772c71c6eb
Furacin <manzanile@gmail.com>
2017-02-19 23:43:23 +0100

    Nueva versión (2) de la aplicación. Se ha implementado gráficos en la segunda pantalla del ViewPager, usando la librería proporcionada por MPAndroidChart.

--- app/src/main/java/com/furazin/android/calorias/SecondFragment.java
+++ app/src/main/java/com/furazin/android/calorias/SecondFragment.java
@@ -1,20 +1,30 @@
 package com.furazin.android.calorias;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.furazin.android.calorias.grafica.MyMarkerView;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 
 /**
  * Created by manza on 17/02/2017.
  */
 
-public class SecondFragment extends Fragment {
+public class SecondFragment extends SimpleFragment {
     public static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
+//    Button estadistica;
+
+    private BarChart mChart;
 
     public static SecondFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -27,9 +37,53 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.second_page, container, false);
+//        View view = inflater.inflate(R.layout.second_page, container, false);
 
-        return view;
+        View v = inflater.inflate(R.layout.frag_simple_bar, container, false);
+
+//        estadistica = (Button) view.findViewById(R.id.boton_estadistica);
+//        estadistica.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getActivity(), ListViewBarChartActivity.class);
+//                startActivity(i);
+//            }
+//        });
+
+        // create a new chart object
+        mChart = new BarChart(getActivity());
+        mChart.getDescription().setEnabled(false);
+//        mChart.setOnChartGestureListener(this);
+
+        MyMarkerView mv = new MyMarkerView(getActivity(), R.layout.custom_marker_view);
+        mv.setChartView(mChart); // For bounds control
+        mChart.setMarker(mv);
+
+        mChart.setDrawGridBackground(false);
+        mChart.setDrawBarShadow(false);
+
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(),"OpenSans-Light.ttf");
+
+        mChart.setData(generateBarData(1, 20000, 12));
+
+        Legend l = mChart.getLegend();
+        l.setTypeface(tf);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTypeface(tf);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        mChart.getAxisRight().setEnabled(false);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        // programatically add the chart
+        FrameLayout parent = (FrameLayout) v.findViewById(R.id.parentLayout);
+        parent.addView(mChart);
+
+        return v;
     }
 
     @Override
@@ -46,4 +100,6 @@ public class SecondFragment extends Fragment {
         super.onCreateOptionsMenu(menu,inflater);
         inflater.inflate(R.menu.menu_main,menu);
     }
+
+
 }
